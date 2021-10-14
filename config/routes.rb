d0bdd37e85329c28f06aa8b2218667af773e722d
Gail Eddy <gaileddy68@gmail.com>
2021-10-14 10:07:23 -0500

    add actors

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/actor_path", controller: "actors_controller", action: "actor_method"
 end
