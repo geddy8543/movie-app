f12f7705a22391690fae1044690963f47ecc1cf2
Gail Eddy <gaileddy68@gmail.com>
2021-10-15 09:38:50 -0500

    Add movie model and enter movies with console

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -3,4 +3,10 @@ class ActorsController < ApplicationController
     actor = Actor.all
     render json: actor.as_json
   end
+
+  def single_actor
+    single_actor = params[:id]
+    actor = Actor.find_by(id: single_actor)
+    render json: actor.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/actor_path", controller: "actors_controller", action: "all_actors"
+  get "/actor_path", controller: "actors", action: "all_actors"
+  get "/single_actor/:id", controller: "actors", action: "single_actor"
 end
