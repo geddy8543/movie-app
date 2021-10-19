f91e3a8fbf0f49628bb7f696eccbf2935c2a1f78
Gail Eddy <gaileddy68@gmail.com>
2021-10-19 08:07:10 -0500

    add route controller actions

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,4 @@
 class MoviesController < ApplicationController
+  get "/all_movies" => "movies#all_movies"
+  get "/one_movie" => "movies#one_movie"
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,12 @@
 class Movie < ApplicationRecord
+  def all_movies
+    movies = Movie.all
+    return movies
+  end
+
+  def one_movie
+    movie_id = params[:id]
+    movie = Movie.find_by(id: movie_id)
+    render json: movie.as_json
+  end
 end
