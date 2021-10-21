0a9a57892e3185df8d8235ef1d0abd5e84494da1
Gail Eddy <gaileddy68@gmail.com>
2021-10-21 08:57:15 -0500

    add to controller for movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,12 +1,2 @@
 class Movie < ApplicationRecord
-  def index
-    movies = Movie.all
-    render json: movies.as_json
-  end
-
-  def show
-    movie_id = params[:id].to_i
-    movie = Movie.find_by(id: movie_id)
-    render json: movie.as_json
-  end
 end
