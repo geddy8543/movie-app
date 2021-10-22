55492f14851e786ad5adc91fa52a506eed2dc753
Gail Eddy <gaileddy68@gmail.com>
2021-10-22 10:27:07 -0500

    adds validations

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,5 @@
 class Actor < ApplicationRecord
+  validates :first_name, presence: true, length: { greater_than: 2 }
+  validates :last_name, length: { greater_than: 2 }
+  validates :known_for, presence: true
 end
